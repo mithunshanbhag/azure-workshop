66b7389148a0e11c53e6562b5cb157ccc6cb172a
mithun shanbhag <mithunshanbhag@hotmail.com>
2020-08-30 05:28:58 +0530

    placeholder for SAS demo

---
+++ code-samples/blob-storage-sas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureFundamentalsWorkshop.CodeSamples.BlobStorage
+{
+    class BlobStorageSas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
