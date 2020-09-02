ef4ff6fa9fff899af0dad8bb20cdf19e18795c82
mithun shanbhag <mithunshanbhag@hotmail.com>
2020-09-02 18:30:49 +0530

    placeholder commit for blob triggered function

---
+++ code-samples/function-app-mixed/BlobTriggeredFunction.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace function_app_mixed
+{
+    public static class BlobTriggeredFunction
+    {
+        [FunctionName("BlobTriggeredFunction")]
+        public static void Run(
+            [BlobTrigger("samples-workitems/{name}", Connection = "")] Stream myBlob,
+            string name,
+            ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
