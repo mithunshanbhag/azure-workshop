b6ed4abd8d103f81fa5eaeb84d2b4c11ad2ca527
mithun shanbhag <mithunshanbhag@hotmail.com>
2020-09-09 20:52:00 +0530

    placeholder for cosmos sample

---
+++ code-samples/cosmos-db-basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cosmos_db_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
